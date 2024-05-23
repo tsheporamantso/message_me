eee5a85b359d5f9f73c21abe579530dea6a570a7
tsheporamantso <tshepo.ramantso@outlook.com>
2024-05-23 10:59:41 +0200

    Correct rubocop offences

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,4 +1,3 @@
 class ChatroomController < ApplicationController
-  def index
-  end
+  def index; end
 end
--- test/controllers/chatroom_controller_test.rb
+++ test/controllers/chatroom_controller_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class ChatroomControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
