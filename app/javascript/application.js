faf8af8fb74bfd3adbe2ac27e3ac4e7a605c7778
tsheporamantso <tshepo.ramantso@outlook.com>
2024-05-23 12:26:23 +0200

    Install and configure semantic ui

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
-//= require jquery
-//= require semantic-ui
-
 import "@hotwired/turbo-rails"
 import "controllers"
+import * as jquery from "jquery"
+import "semantic-ui"
+
+$(document).on('turbo:load', function() {
+  console.log('loaded turbo links')
+  $('.ui.dropdown').dropdown()
+});
