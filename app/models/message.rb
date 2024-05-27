7780e2204744c7b7a3837a2dfd49b3b91f3fb086
tsheporamantso <tshepo.ramantso@outlook.com>
2024-05-27 16:38:23 +0200

    Add message validation and association

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,4 @@
 class Message < ApplicationRecord
+  validates :message, presence: true
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { in: 3..25 }
   has_secure_password
+  has_many :messages
 end
