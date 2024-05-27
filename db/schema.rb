c909eb7e5b7cdc1a1d19e49d1d37005b903ad396
tsheporamantso <tshepo.ramantso@outlook.com>
2024-05-27 16:53:51 +0200

    Correct typo on message validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_27_132558) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_27_143548) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
