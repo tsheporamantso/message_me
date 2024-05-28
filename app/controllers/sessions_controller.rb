e6618935808c1e770a1ab27150f75611c76944e5
tsheporamantso <tshepo.ramantso@outlook.com>
2024-05-28 16:41:54 +0200

    correct linter errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,9 @@
 class SessionsController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
     user = User.find_by(username: params[:session][:username])
-    if user && user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
       session[:user_id] = user.id
       flash[:success] = 'You have successfully logged in'
       redirect_to root_path
