0fb42d05f045158b786cb6662e4f7806cf5281b6
tsheporamantso <tshepo.ramantso@outlook.com>
2024-05-28 10:48:37 +0200

    Correct rubocop offences

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,9 +10,9 @@ class ApplicationController < ActionController::Base
   end
 
   def require_user
-    if !logged_in?
-      flash[:error] = 'You must be logged in to perform that action'
-      redirect_to login_path
-    end
+    return if logged_in?
+
+    flash[:error] = 'You must be logged in to perform that action'
+    redirect_to login_path
   end
 end
