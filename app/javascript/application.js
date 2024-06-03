37e9e0493b196f08b8596eea9b549238ecb7ea9f
tsheporamantso <tshepo.ramantso@outlook.com>
2024-06-03 10:59:09 +0200

    Correct JS function to close or dismiss flash message block

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -8,8 +8,12 @@ import "semantic-ui"
 $(document).on('turbo:load', function() {
   console.log('loaded turbo links')
   $('.ui.dropdown').dropdown()
+  $('.message .close').on('click', function() {
+    $(this).closest('.message').transition('fade');
+  });
 });
 
+ 
 // $(document).on('turbo:load', function() {
 //   console.log('loaded turbo links')
 //   $('.ui.dropdown').dropdown();
